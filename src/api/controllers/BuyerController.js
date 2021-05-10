5d11e65874bebf8f9e8c4ed85a3de37e519254d2
unknown <nguyenhoaibaoltt@gmail.com>
2021-05-10 20:18:57 +0700

    edit api

--- src/api/controllers/BuyerController.js
+++ src/api/controllers/BuyerController.js
@@ -119,6 +119,20 @@ class BuyerController {
 
     verifyaccount(req,res,next)
     {
+         // Website you wish to allow to connect
+        res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4040');
+
+        // Request methods you wish to allow
+        res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+        // Request headers you wish to allow
+        res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+        // Set to true if you need the website to include cookies in the requests sent
+        // to the API (e.g. in case you use sessions)
+        res.setHeader('Access-Control-Allow-Credentials', true);
+
+
         var username = req.body.username;
         var password = req.body.password;
         const buyer = new Buyer(req.body);
