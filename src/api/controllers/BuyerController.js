109e09dbec47fede241511d175c36cc97170a106
unknown <nguyenhoaibaoltt@gmail.com>
2021-05-10 20:48:17 +0700

    edit api

--- src/api/controllers/BuyerController.js
+++ src/api/controllers/BuyerController.js
@@ -120,7 +120,7 @@ class BuyerController {
     verifyaccount(req,res,next)
     {
          // Website you wish to allow to connect
-        res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4040');
+        res.setHeader('Access-Control-Allow-Origin', '*');
 
         // Request methods you wish to allow
         res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
--- src/index.js
+++ src/index.js
@@ -55,6 +55,13 @@ app.use(methodOverride('_method'));
 
 app.use(cookieParser('hoaibao123'));
 
+app.use(function (req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  next();});
+
 app.use(sortMiddleware);
 
 app.use(sessionMiddleware);
