e2bc0cf525fb72ee48dd2b54a87310c0cd0b41b9
unknown <nguyenhoaibaoltt@gmail.com>
2021-05-10 21:17:52 +0700

    edit api

--- src/api/controllers/BuyerController.js
+++ src/api/controllers/BuyerController.js
@@ -123,14 +123,14 @@ class BuyerController {
         res.setHeader('Access-Control-Allow-Origin', '*');
 
         // Request methods you wish to allow
-        res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+        // res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
 
         // Request headers you wish to allow
         res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
 
         // Set to true if you need the website to include cookies in the requests sent
         // to the API (e.g. in case you use sessions)
-        res.setHeader('Access-Control-Allow-Credentials', true);
+        // res.setHeader('Access-Control-Allow-Credentials', true);
 
 
         var username = req.body.username;
--- src/index.js
+++ src/index.js
@@ -57,9 +57,9 @@ app.use(cookieParser('hoaibao123'));
 
 app.use(function (req, res, next) {
   res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-  res.setHeader('Access-Control-Allow-Credentials', true);
+  // res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  // res.setHeader('Access-Control-Allow-Credentials', true);
   next();});
 
 app.use(sortMiddleware);
